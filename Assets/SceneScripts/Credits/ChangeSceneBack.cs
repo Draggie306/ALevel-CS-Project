abfec18ca0bbc94dbb650cb69b6b0a6c89dc2183
Draggie <75524491+Draggie306@users.noreply.github.com>
2024-03-05 10:56:55 +0000

    Add todo in ChangeSceneBack.cs

--- Assets/SceneScripts/Credits/ChangeSceneBack.cs
+++ Assets/SceneScripts/Credits/ChangeSceneBack.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeSceneBack : MonoBehaviour
+
+// TODO (05/03/2024): Please make this not log the user out, if they are logged in when they click the credits, make sure 
+// that the scope can be interacted with on click. Not sure how to make this persistent, use playerprefs if needed
 {
     public bool isLoggedOut = true;
 

0634c8a57d6f76caec94d3f6edc31ce2fdf320f2
Draggie306 <75524491+Draggie306@users.noreply.github.com>
2024-08-04 21:06:51 +0100

    Auto login correct

--- Assets/SceneScripts/LoginScreen/NewLoginFlow.cs
+++ Assets/SceneScripts/LoginScreen/NewLoginFlow.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -52,9 +55,21 @@ public class NewLoginFlow : MonoBehaviour
         UpdateMainScreenText("Press any key to start");
     }
 
-    private void Update()
+    private async void Update()
     {
-        // Dev setting
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            // hack fix for tabbing between fields - just select pasword lol
+            GameObject.Find("TMP_Password").GetComponent<TMP_InputField>().Select();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            // Simulate click on login button
+            LoginButton.GetComponent<Button>().onClick.Invoke();
+        }
+
+        // Dev setting (fast)
         if (!HasCliced) {
             StartCoroutine(StartSequence());
             HasCliced = true;
@@ -103,7 +118,7 @@ public class NewLoginFlow : MonoBehaviour
 
         UpdateMainScreenText("");
         UpdateLegacyInfoText("");
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.1f);
         // UpdateMainScreenText("Assets downloaded. Starting game...");
 
         //SceneManager.LoadScene("MainMenu");
@@ -124,19 +139,45 @@ public class NewLoginFlow : MonoBehaviour
         }
         else
         {
-            UpdateMainScreenText("Error logging in.");
-            UpdateLegacyInfoText("Error logging in. Please try again later.", "FF0000");
+            Debug.LogError("[NewLoginFlow] Error logging in.");
+            UpdateMainScreenText("Retrying in 5...");
+            yield return new WaitForSeconds(1);
+            UpdateMainScreenText("Retrying in 4...");
+            yield return new WaitForSeconds(1);
+            UpdateMainScreenText("Retrying in 3...");
+            yield return new WaitForSeconds(1);
+            UpdateMainScreenText("Retrying in 2...");
+            yield return new WaitForSeconds(1);
+            UpdateMainScreenText("Retrying in 1...");
+            yield return new WaitForSeconds(1);
+            yield return StartCoroutine(RunAsync(async () => loginSuccess = await NetLogin())); // <- AI generated
+
+            if (loginSuccess)
+            {
+                UpdateMainScreenText("Logged in. Starting game...");
+                UpdateLegacyInfoText("Starting game...");
+                //yield return new WaitForSeconds(1);
+                SceneManager.LoadScene("MainMenu");
+            }
+            else
+            {
+                UpdateMainScreenText("Error");
+                UpdateLegacyInfoText("Error logging in. Please restart and try again.", "FF0000");
+            }
         }
     }
-
+    private bool AlwaysShowLoginUi = false;
     private bool HasShownPleaseLogin = false;
 
     private async Task<bool> NetLogin() {
         // Attempt to detect if the user is already logged in and has a token saved in %appdata%
+        Debug.Log($"[NetLogin] triggered. Flags: AlwaysShowLoginUiTest: {AlwaysShowLoginUi}, HasShownPleaseLogin: {HasShownPleaseLogin}");
         
         string directory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Draggie", "Saturnian");
+        // AlwaysShowLoginUi = false;
+        bool DirExists = true;
 
-        if (!Directory.Exists(directory))
+        if (!Directory.Exists(directory) && !AlwaysShowLoginUi) // If the flag AlwaysShowLoginUiTest is set, ignore this selection statement.
         {
             if (!HasShownPleaseLogin)
             {
@@ -145,7 +186,137 @@ public class NewLoginFlow : MonoBehaviour
             }
             // Directory does not exist; the user has not logged in before via the Launcher.
             Debug.Log($"[NetLogin] Draggie directory does not exist at {directory}");
+            AlwaysShowLoginUi = true;
+            DirExists = false;
+        } else {
+            Debug.Log($"[NetLogin] Draggie directory exists at {directory}");
+        }
+
+        if (DirExists && !AlwaysShowLoginUi)
+        {
+            // Check if the user has a token saved in %appdata%
+            string tokenPath = Path.Combine(directory, "token.bin");
+
+            if (File.Exists(tokenPath))
+            {
+                AlwaysShowLoginUi = true;
+                UpdateMainScreenText("Verifying credentials...");
+
+                string username = Environment.UserName.ToLower() + ".3d060a9b-f248-4e2b-babd-e6d5d2c2ab8b";
+
+                using SHA256 sha256 = SHA256.Create();
+                byte[] hashKey = sha256.ComputeHash(Encoding.UTF8.GetBytes(username));
+                byte[] fernet_key = hashKey;
+
+                string encryptedToken = File.ReadAllText(tokenPath);
+
+                Debug.Log($"[NetLogin] Attempting to decrypt token {encryptedToken} with key {fernet_key}");
+                UpdateMainScreenText("Decrypting token...");
+                var decryptedToken = DecryptFernet(fernet_key, encryptedToken, out DateTime timestamp);
+                Debug.Log($"[NetLogin] Decrypted token: {decryptedToken}");
+
+                // Decrypt token and send to server
+                string endpoint = $"{serverBaseDirectoryUrl}/token_login";
+                string jsonString = JsonConvert.SerializeObject(new { token = decryptedToken, scope = "unity/draggiegames-compsciproject" });
+
+                using HttpClient client = new();
+                UpdateMainScreenText("Sending request...");
+                var response = await client.PostAsync(endpoint, new StringContent(jsonString, Encoding.UTF8, "application/json"));
+
+                var responseString = await response.Content.ReadAsStringAsync();
+                var responseStatusCode = response.StatusCode;
+                
+                if (responseStatusCode == HttpStatusCode.OK)
+                {
+                    Debug.Log($"[NetLogin] Token login successful: {responseString}");
+                    UpdateMainScreenText("Checking validity...");
+
+                    string validation_endpoint = $"{serverBaseDirectoryUrl}/api/v1/saturnian/game/gameData/licenses/validation"; // GET request
+
+                    using HttpClient validationClient = new();
+                    validationClient.DefaultRequestHeaders.Add("token", decryptedToken);
+                    validationClient.DefaultRequestHeaders.Add("User-Agent", "unity/draggiegames-compsciproject");
+                    dynamic validationResponse = await validationClient.GetAsync(validation_endpoint);
+
+                    if (validationResponse.StatusCode != HttpStatusCode.OK)
+                    {
+                        try {
+                            string message = JObject.Parse(await validationResponse.Content.ReadAsStringAsync()).message;
+                            Debug.LogError($"[NetLogin] Validation failed: {message}");
+                            UpdateLegacyInfoText($"Token validation failed: {message}", "FF0000");
+                            return false;
+                        } catch (JsonReaderException e) {
+                            Debug.LogError($"[NetLogin] Error parsing error JSON, likely server error: {e.Message}");
+                        }
+                        Debug.LogError($"[NetLogin] Validation failed: {validationResponse.StatusCode}");
+                        UpdateLegacyInfoText("Validation failed. Please try again later.", "FF0000");
+                        return false;
+                    }
+                    var validationResponseString = await validationResponse.Content.ReadAsStringAsync();
+                    Debug.Log($"[NetLogin] Validation response: {validationResponseString}");
+
+                    // Parse response. 
+                    dynamic parsedValidationResponse = JObject.Parse(validationResponseString);
+                    var entitlements = parsedValidationResponse.entitlements;
+
+                    // saturnian_alpha_tester || saturnian_beta_tester are the entitlements we are looking for
+                    bool isAlphaTester = false;
+                    bool isBetaTester = false;
+                    string TesterMsg = "";
+
+                    Debug.Log($"[NetLogin] Entitlements:\n{entitlements}");
+
+                    foreach (JProperty entitlement in entitlements)
+                    {
+                        string EntitlementName = entitlement.Name;
+                        Debug.Log($"[NetLogin] Entitlement: {entitlement}");
+
+                        if (EntitlementName == "saturnian_alpha_tester") {
+                            isAlphaTester = true;
+                            TesterMsg = "You are an ALPHA tester! Thank you for testing Saturnian! Don't forget to report bugs!";
+                            break;
+                        } else if (EntitlementName == "saturnian_beta_tester") {
+                            isBetaTester = true;
+                            TesterMsg = "You are a beta tester! Thank you for testing Saturnian! Don't forget to report bugs!";
+                            break;
+                        } else {
+                            Debug.Log($"[NetLogin] No known tester entitlements: {entitlement}");
+                        }
+                    }
+
+                    if (isAlphaTester || isBetaTester)
+                    {
+                        Debug.Log($"[NetLogin] User is alpha tester: {isAlphaTester}, beta tester: {isBetaTester}");
+                        UpdateMainScreenText($"Welcome back, {parsedValidationResponse.username}! {TesterMsg}");
+                        UpdateLegacyInfoText("Login successful!", "00ACFF");
+                        await Task.Delay(2600);
+                        return true;
+                    }
+                    else
+                    {
+                        Debug.LogError("[NetLogin] User is not an alpha or beta tester.");
+                        UpdateLegacyInfoText("You are not an alpha or beta tester.", "FF0000");
+                        return false;
+                    }
+                }
+            else
+            {
+                // Token does not exist; the user has not logged in before.
+                Debug.Log($"[NetLogin] Token does not exist at {tokenPath}");
+                AlwaysShowLoginUi = true;
+            }
+            AlwaysShowLoginUi = true;
+        } else {
+            Debug.Log($"[NetLogin] Draggie directory does not exist at {directory}");
+            AlwaysShowLoginUi = true;
+        }
+        }
+
+        if (AlwaysShowLoginUi)
+        {
+            Debug.Log("[NetLogin] Showing login UI.");
             bool showLoginButtonUi = await ShowLoginButtonUi();
+            LoginButton.GetComponent<Button>().interactable = true;
             if (!showLoginButtonUi) {
                 Debug.LogError("[NetLogin] Error showing login button UI.");
                 return false;
@@ -189,15 +360,158 @@ public class NewLoginFlow : MonoBehaviour
                     
                     // Retry by calling this function again after 3 seconds
                     GameObject.Find("LoginText").GetComponent<TextMeshProUGUI>().text = "Log in";
+                    LoginButton.GetComponent<Button>().interactable = false; // don't confuse the user by allowing spam for 3 seconds
                     await Task.Delay(3000);
                     return await NetLogin();
                 }
             }
-        } else {
-            Debug.Log($"[NetLogin] Draggie directory exists at {directory}");
         }
-        return true;
+
+        UpdateLegacyInfoText("A uncorrectable error occurred. Please try again later; report this to the developers.", "FF0000");
+        return false;
     }
+    
+    /// 
+    /// Token decryption from StackOverflow: https://stackoverflow.com/a/50844957
+    /// 
+
+    // Token is base64 url encoded
+    public static string DecryptFernet(byte[] key, string token, out DateTime timestamp)
+    {
+        if (key == null)
+        {
+            throw new ArgumentNullException(nameof(key));
+        }
+
+        if (key.Length != 32)
+        {
+            throw new ArgumentException(nameof(key));
+        }
+
+        if (token == null)
+        {
+            throw new ArgumentNullException(nameof(token));
+        }
+
+        byte[] token2 = Base64UrlDecode(token);
+
+        if (token2.Length < 57)
+        {
+            throw new ArgumentException(nameof(token));
+        }
+
+        byte version = token2[0];
+
+        if (version != 0x80)
+        {
+            throw new Exception("version");
+        }
+
+        // Check the hmac
+        {
+            byte[] signingKey = new byte[16];
+            Buffer.BlockCopy(key, 0, signingKey, 0, 16);
+
+            using (var hmac = new HMACSHA256(signingKey))
+            {
+                hmac.TransformFinalBlock(token2, 0, token2.Length - 32);
+                byte[] hash2 = hmac.Hash;
+
+                IEnumerable<byte> hash = token2.Skip(token2.Length - 32).Take(32);
+
+                if (!hash.SequenceEqual(hash2))
+                {
+                    throw new Exception("Wrong HMAC!");
+                }
+            }
+        }
+
+        {
+            // BigEndian to LittleEndian
+            long timestamp2 = BitConverter.ToInt64(token2, 1);
+            timestamp2 = IPAddress.NetworkToHostOrder(timestamp2);
+
+            timestamp = DateTimeOffset.FromUnixTimeSeconds(timestamp2).UtcDateTime;
+        }
+
+        byte[] decrypted;
+
+        using (var aes = new AesManaged())
+        {
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            byte[] encryptionKey = new byte[16];
+            Buffer.BlockCopy(key, 16, encryptionKey, 0, 16);
+            aes.Key = encryptionKey;
+
+            byte[] iv = new byte[16];
+            Buffer.BlockCopy(token2, 9, iv, 0, 16);
+            aes.IV = iv;
+
+            using (var decryptor = aes.CreateDecryptor())
+            {
+                const int startCipherText = 25;
+                int cipherTextLength = token2.Length - 32 - 25;
+                decrypted = decryptor.TransformFinalBlock(token2, startCipherText, cipherTextLength);
+            }
+        }
+
+        string FinalString = Encoding.UTF8.GetString(decrypted);
+        Debug.Log($"[DecryptFernet] Decrypted token: {FinalString}");
+
+        return FinalString;
+    }
+
+    public static byte[] Base64UrlDecode(string s)
+    {
+        // https://stackoverflow.com/a/26354677/613130
+        // But totally rewritten :-)
+
+        char[] chars;
+
+        switch (s.Length % 4)
+        {
+            case 2:
+                chars = new char[s.Length + 2];
+                chars[chars.Length - 2] = '=';
+                chars[chars.Length - 1] = '=';
+                break;
+            case 3:
+                chars = new char[s.Length + 1];
+                chars[chars.Length - 1] = '=';
+                break;
+            default:
+                chars = new char[s.Length];
+                break;
+        }
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            switch (s[i])
+            {
+                case '_':
+                    chars[i] = '/';
+                    break;
+                case '-':
+                    chars[i] = '+';
+                    break;
+                default:
+                    chars[i] = s[i];
+                    break;
+            }
+        }
+
+        byte[] result = Convert.FromBase64CharArray(chars, 0, chars.Length);
+        return result;
+    }
+
+
+    /// 
+    /// End of token decryption from StackOverflow ///
+    /// 
+
+
 
     public async Task<initialUsernameJsonDict> WaitForButtonPress()
     {
@@ -205,6 +519,7 @@ public class NewLoginFlow : MonoBehaviour
     
         // Assuming you have a reference to the login button
         Button loginButton = LoginButton.GetComponent<Button>();
+
         loginButton.onClick.AddListener(async () => {
             Debug.Log("[WaitForButtonPress] Login button clicked.");
             var result = await HandleButtonPressAsync();
@@ -241,7 +556,6 @@ public class NewLoginFlow : MonoBehaviour
         // the TextMeshProUGUI component is just the visual representation of the text. When the TMP_InputField is set to the "password" mode, the TextMeshProUGUI component will only show ***s!
         // Taken from: https://forum.unity.com/threads/change-inputfield-input-from-standard-to-password-text-via-script.291897/
 
-        GameObject.Find("LoginText").GetComponent<TextMeshProUGUI>().text = "Logging in...";
         Debug.Log($"[OnButtonPress] Email: '{email}', Password: '{password}'");
 
         // Sanitise email and password, remove u200b (zero width space) and trim. for some reason, the input field adds a zero width space to the end of the string
@@ -256,6 +570,7 @@ public class NewLoginFlow : MonoBehaviour
             UpdateLegacyInfoText("Please enter an email and password!", "FF0000");
             return null;
         } else {
+            GameObject.Find("LoginText").GetComponent<TextMeshProUGUI>().text = "Logging in...";
             Debug.Log("Email and password seem to have been entered");
             UpdateLegacyInfoText("Attempting to log in...", "00ACFF");
         }
@@ -272,6 +587,8 @@ public class NewLoginFlow : MonoBehaviour
     private async Task<bool> ShowLoginButtonUi()
     {
         Debug.Log("[ShowLoginButtonUi] Showing login button UI.");
+
+        // TODO: fade in/out animations
         LoginButtonUIParent.SetActive(true);
         CentreText.SetActive(false);
         SpinnerIcon.SetActive(false);
